5feaa9229de4cc8639ec6bcaffe6f66f0e8e042f
Krissaane Inès <38727492+ineskris@users.noreply.github.com>
2018-09-26 10:50:46 +0200

    Add files via upload

--- Bash script/Final_shell_script.sh
+++ Bash script/Final_shell_script.sh
@@ -1,1 +1,13 @@
+#!/bin/sh
 
+# Final Script - 13/08/2018 Ines Krissaane
+echo "DEBUT"
+count = 0
+while read W; do
+	while read N; do
+			count=$((count+1))
+			#echo $count n1-standard-4 $N $W
+			sh ./function.sh $count n1-standard-4 $N $W &> output.csv
+	done <nodes_google1.txt
+done <machine_name_google2.txt
+echo "FIN" 
