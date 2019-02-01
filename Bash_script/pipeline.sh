7959eef1fdb7a30ce0ae9b4b492ca42a3d63d51f
Krissaane Inès <38727492+ineskris@users.noreply.github.com>
2019-02-01 13:14:59 -0500

    Update pipeline.sh

--- Bash_script/pipeline.sh
+++ Bash_script/pipeline.sh
@@ -2,8 +2,8 @@
 
 test()
 {
-  count=$1
-  M=$2
+	count=$1
+	M=$2
 	N=$3
 	W=$4
 	start=$(date +"%s")
@@ -12,41 +12,37 @@ test()
 	gcloud dataproc clusters create hail$count \
 		--master-machine-type=$M \
 		--metadata=JAR=gs://hail-common/builds/0.2/jars/hail-0.2-d33e2d1c19b2-Spark-2.2.0.jar,ZIP=gs://hail-common/builds/0.2/python/hail-0.2-d33e2d1c19b2.zip,MINICONDA_VERSION=4.4.10 \
-		--project=copd-genes \
+		--project=your-project \
 		--num-workers=2 \
 		--worker-machine-type=$W \
-		--zone=us-east1-d \
+		--zone=your-zone \
 		--image-version=1.2-deb9 \
 		--num-preemptible-workers=$N \
-    --network=ines-hail \
-		--initialization-actions=gs://dataproc-initialization-actions/conda/bootstrap-conda.sh,gs://script-cluster/script.py
+    		--network=your-network \
+		--initialization-actions=gs://dataproc-initialization-actions/conda/bootstrap-conda.sh,gs://your-bucket/script.py, gs://install_datadog.sh 
 
 
 	#submit job (48hours max) timeout 172800s
-  gcloud dataproc jobs submit pyspark gs://last-results/gwas-sex-1kgP1.py --cluster=hail$count --project=copd-genes
+ 	gcloud dataproc jobs submit pyspark gs://your-bucket/your-python-file.py --cluster=hail$count --project=your-project
 
-	#change the name file
-	# gsutil mv gs://ines-results/time.csv gs://ines-results/hail-time${count}.csv
 
 	# #metrics extraction
 	# 	#for the master node from datadog
-	# python Datadog_extraction.py "$start" "$(date +"%s")" "hail${count}-m.c.avl-hail-ines.internal"
+	# python Datadog_extraction.py "$start" "$(date +"%s")" "hail${count}-m.c.your-project.internal"
 
-	# gsutil cp hail${count}-m.c.avl-hail-ines.internal.csv gs://ines-results/hail${count}-master-datadog.csv
+	# gsutil cp hail${count}-m.c.your-project.internal.csv gs://your-bucket/hail${count}-master-datadog.csv
 	# 	#for the worker nodes do a for loop
 	# for i in $(seq 0 $((${N}-1)));
 	# do
-	# 	 python Datadog_extraction.py "$start" "$(date +"%s")" "hail${count}-w-$i.c.avl-hail-ines.internal"
-	# 	 gsutil cp hail${count}-w-$i.c.avl-hail-ines.internal.csv gs://ines-results/hail$count-worker-$i-datadog.csv;
+	# 	 python Datadog_extraction.py "$start" "$(date +"%s")" "hail${count}-w-$i.c.your-project.internal"
+	# 	 gsutil cp hail${count}-w-$i.c.your-project.internal.csv gs://your-bucket/hail$count-worker-$i-datadog.csv;
 	# done
 
 	#delete cluster
-  gcloud dataproc clusters delete hail$count -q
+  	gcloud dataproc clusters delete hail$count -q
 
-	echo "$count,$start,$(date +"%s"),$N,$W" >> final_conf_cluster-1KGphase1-sex.csv
+	echo "$count,$start,$(date +"%s"),$N,$W" >> final_conf_cluster.csv
 
-
-	#gsutil cp conf_cluster.csv gs://ines-work/conf_cluster.csv;
 }
 
 test $1 $2 $3 $4
