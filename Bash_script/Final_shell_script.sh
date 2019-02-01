e04a2a5465134257ccaa6fe2f6fc6fb414326157
Krissaane Inès <38727492+ineskris@users.noreply.github.com>
2019-02-01 13:10:51 -0500

    Update Final_shell_script.sh

--- Bash_script/Final_shell_script.sh
+++ Bash_script/Final_shell_script.sh
@@ -7,7 +7,7 @@ while read W; do
 	while read N; do
 			count=$((count+1))
 			#echo $count n1-standard-4 $N $W
-			sh ./function.sh $count n1-standard-4 $N $W &> output.csv
-	done <nodes_google1.txt
-done <machine_name_google2.txt
+			sh ./pipeline.sh $count n1-standard-4 $N $W &> output.csv
+	done <nodes_google.txt
+done <machine_name_google.txt
 echo "FIN" 
